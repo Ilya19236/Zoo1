a05855a59a1e04fc89498b47949107255fbbd503
Ilya <Sans19236bat@gmail.com>
2022-12-05 22:57:08 +0300

    Start

---
+++ Leopard.py
@@ -0,0 +1,52 @@
+class FarEastLeopard:
+    def __init__(self, name, age, consumesFoodPerDay, mass):
+        self.__nameOfView = "Дальневосточный Леопард"
+        self.__biom = "Дальний Восток"
+        self.__squarePerOne = 10
+        self.__ration = ['косуля', 'кабан', 'пятнистый олень', 'фазан', 'заяц', 'барсук']
+        self.__sound = "Рррр"
+        self.__name = name
+        self.__consumesFoodPerDay = consumesFoodPerDay
+        self.__age = age
+        self.__mass = mass
+        self.__necessaryFoodVulumeForOneFiding = consumesFoodPerDay / 3
+    @property
+    def nameOfView(self):
+        return self.__nameOfView
+    @property
+    def biom(self):
+        return self.__biom
+    @property
+    def squarePerOne(self):
+        return self.__squarePerOne
+    @property
+    def ration(self):
+        return self.__ration
+    @property
+    def sound(self):
+        return self.__sound
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def consumesFoodPerDay(self):
+        return self.__consumesFoodPerDay
+    @property
+    def age(self):
+        return self.__age
+    @property
+    def mass(self):
+        return self.__mass
+    def Play(self):
+        print(self.__name, " поигрался с хвостиком.")
+    def MakeSound(self):
+        print(self.__name, ": ", self.__sound)
+    def Eat(self, food, portion):
+        self.thePreviousMass = self.__mass
+        if food in self.__ration:
+            print(self.__name, " покушал.")
+            self.__mass += portion
+        else:
+            print(self.__name, "отказался кушать. Данная еда не входит в рацион Дальневосточных леопардов.")
+    def IsFeeded (self):
+        return self.thePreviousMass + self.__necessaryFoodVulumeForOneFiding <= self.__mass
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Zoo1
+from Tiger import *
+from Leopard import *
+from Alabay import *
+
+Andrey = AmurTiger('Андрей', 7, 9, 340)
+Eugene = FarEastLeopard ("Евгений", 2, 6, 45)
+Petya = Alabay("Петя", 3, 3, 70)
+
+Andrey.MakeSound()
+Eugene.MakeSound()
+Petya.MakeSound()
+
+Andrey.Play()
+Eugene.Play()
+Petya.Play()
+
+Andrey.Eat('банан', 3)
+Eugene.Eat('кабан', 2)
+Petya.Eat('говядина', 1)
+
+print(Andrey.IsFeeded())
+print(Eugene.IsFeeded())
+print(Petya.IsFeeded())
